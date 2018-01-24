b623fc9b63684885e8f408d8a51bc1ddff890816
Craig Fowler <craig@csf-dev.com>
2018-01-24 20:22:08 +0000

    Remove some unneeded supporting files

--- Tools/Appveyor.before_build.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-git submodule update --init --recursive
-
-nuget restore CSF.Screenplay.sln
-
-copy /y Tests\CSF.Screenplay.Web.Tests\App.AppVeyor.config Tests\CSF.Screenplay.Web.Tests\App.config
-
-@echo off
