5af03f446731d110369605f2e2647c368f0c903c
Craig Fowler <craig@csf-dev.com>
2018-02-08 19:48:27 +0000

    Resolve #28 - Add extension method

--- CSF.WebDriverExtras/WebDriverExtensions.cs
+++ CSF.WebDriverExtras/WebDriverExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CSF.WebDriverExtras.BrowserId;
+using OpenQA.Selenium;
+
 namespace CSF.WebDriverExtras
 {
   /// <summary>
@@ -6,6 +9,22 @@ namespace CSF.WebDriverExtras
   /// </summary>
   public static class WebDriverExtensions
   {
-    
+    static readonly IGetsBrowserIdentification browserIdFactory;
+
+    /// <summary>
+    /// Gets identification information about the given web driver.
+    /// </summary>
+    /// <returns>The browser identification.</returns>
+    /// <param name="webDriver">Web driver.</param>
+    public static BrowserIdentification IdentifyBrowser(this IWebDriver webDriver)
+      => browserIdFactory.GetIdentification(webDriver as IHasCapabilities);
+
+    /// <summary>
+    /// Initializes the <see cref="T:CSF.WebDriverExtras.WebDriverExtensions"/> class.
+    /// </summary>
+    static WebDriverExtensions()
+    {
+      browserIdFactory = new BrowserIdentificationFactory();
+    }
   }
 }
