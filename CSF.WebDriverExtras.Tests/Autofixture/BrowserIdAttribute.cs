6e9d2527597cb2e163b855d19787b39b230e23ed
Craig Fowler <craig@csf-dev.com>
2018-02-11 15:23:12 +0000

    WIP #32 - Create customisations for AutoFixture
    This is in order to declaratively specify a web driver with
    capabilities that has browser identification information.

---
+++ CSF.WebDriverExtras.Tests/Autofixture/BrowserIdAttribute.cs
@@ -0,0 +1,102 @@
+﻿//
+// BrowserIdAttribute.cs
+//
+// Author:
+//       Craig Fowler <craig@csf-dev.com>
+//
+// Copyright (c) 2018 Craig Fowler
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Reflection;
+using OpenQA.Selenium;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.NUnit3;
+
+namespace CSF.WebDriverExtras.Tests.Autofixture
+{
+  public class BrowserIdAttribute : CustomizeAttribute
+  {
+    public string Browser { get; set; }
+
+    public string Version { get; set; }
+
+    PlatformType? platform;
+    public PlatformType Platform
+    {
+      get {
+        return platform ?? PlatformType.Any;
+      }
+      set {
+        platform = value;
+      }
+    }
+
+    public bool HasRequestedVersion { get; set; }
+
+    string requestedVersion;
+    public string RequestedVersion
+    {
+      get {
+        return requestedVersion;
+      }
+      set {
+        requestedVersion = value;
+
+        if(!String.IsNullOrEmpty(value))
+          HasRequestedVersion = true;
+      }
+    }
+
+    public override ICustomization GetCustomization(ParameterInfo parameter)
+    {
+      var webDriverType = GetSanitisedWebDriverType(parameter);
+      return GetCustomisation(webDriverType);
+    }
+
+    Type GetSanitisedWebDriverType(ParameterInfo parameter)
+    {
+      if(parameter == null)
+        throw new ArgumentNullException(nameof(parameter));
+
+      var paramType = parameter.ParameterType;
+
+      if(!typeof(IHasCapabilities).IsAssignableFrom(paramType))
+        throw new ArgumentException($"Parameter type must implement {nameof(IHasCapabilities)}.", nameof(parameter));
+      
+      if(!paramType.IsInterface)
+        throw new ArgumentException($"The parameter type must be an interface, but `{paramType.FullName}' is not.", nameof(parameter));
+
+      return paramType;
+    }
+
+    ICustomization GetCustomisation(Type webDriverType)
+    {
+      var type = typeof(WebDriverWithIdentificationCustomiszation<>);
+      var genericType = type.MakeGenericType(webDriverType);
+
+      return (ICustomization) Activator.CreateInstance(genericType,
+                                                       Browser,
+                                                       Version,
+                                                       platform,
+                                                       HasRequestedVersion,
+                                                       RequestedVersion);
+    }
+  }
+}
